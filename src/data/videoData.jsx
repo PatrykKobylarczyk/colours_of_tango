f147bed4a23df4dcb0cd24373f0eb493c521b0e9
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-12-06 16:41:25 +0100

    change video data

--- src/data/videoData.jsx
+++ src/data/videoData.jsx
@@ -1,27 +1,27 @@
-import tn1 from '../assets/images/tn1.jpg' 
-import tn2 from '../assets/images/tn2.jpg' 
-import tn3 from '../assets/images/tn4.jpg' 
+import tn1 from '../assets/pictures/tn1.png' 
+import tn2 from '../assets/pictures/tn2.png' 
+import tn3 from '../assets/pictures/tn3.png' 
 
 export const videoData = [
     {
       id: 1,
-      author: "Astor Piazzolla",
-      title: "Libertango",
-      url: "https://www.youtube.com/watch?v=hUSAceit-uw",
+      author: "Carlos Gardel",
+      title: "Por una cabeza",
+      url: "https://www.youtube.com/watch?v=CLhIT20QtPY",
       thumbnail: tn1
     },
     {
       id: 2,
-      author: "Astor Piazzolla",
-      title: "S'il vous plaît",
-      url: "https://www.youtube.com/watch?v=8DwXiKOwoqk",
+      author: "Carlos Gardel",
+      title: "Milonga Sentimental",
+      url: "https://www.youtube.com/watch?v=uu_YobRy4Wc",
       thumbnail: tn2
     },
     {
       id: 3,
       author: "Astor Piazzolla",
-      title: "Ave Maria",
-      url: "https://www.youtube.com/watch?v=49f_cjzYufM",
+      title: "Libertango",
+      url: "https://www.youtube.com/watch?v=0IgchM9ORLE",
       thumbnail: tn3
     },
    
