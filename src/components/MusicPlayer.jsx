6f3b3c2edb654c48f290664eadaf32c21a039cbd
Patryk Kobylarczyk <patryk.kobylarczyk@gmail.com>
2023-12-14 00:40:01 +0100

    audio

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -126,7 +126,7 @@ const MusicPlayer = () => {
                 gap={4}
               />
             ) : ( */}
-            <AudioSpectrumAnimation IsMusicInPlayer={IsMusicInPlayer} />
+              <AudioSpectrumAnimation IsMusicInPlayer={IsMusicInPlayer} />
             {/* )} */}
           </div>
 
